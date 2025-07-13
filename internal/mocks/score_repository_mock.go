6ac2e435166f01ef541c780afa76a5ae21d65fd7
Martin Arias <arias95martin@gmail.com>
2025-07-13 19:10:58 -0300

    feat: add score repository mock

---
+++ internal/mocks/score_repository_mock.go
@@ -0,0 +1,40 @@
+package mocks
+
+import (
+	"github.com/Martin-Arias/go-scoring-api/internal/dto"
+	"github.com/Martin-Arias/go-scoring-api/internal/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type ScoreRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *ScoreRepositoryMock) GetScoresByGameID(gameID uint) (*[]dto.PlayerScoreDTO, error) {
+	args := m.Called(gameID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*[]dto.PlayerScoreDTO), args.Error(1)
+}
+
+func (m *ScoreRepositoryMock) GetScoresByPlayerID(playerID uint) (*[]dto.PlayerScoreDTO, error) {
+	args := m.Called(playerID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*[]dto.PlayerScoreDTO), args.Error(1)
+}
+
+func (m *ScoreRepositoryMock) GetScore(playerID, gameID uint) (*model.Score, error) {
+	args := m.Called(playerID, gameID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.Score), args.Error(1)
+}
+
+func (m *ScoreRepositoryMock) SubmitScore(score model.Score) error {
+	args := m.Called(score)
+	return args.Error(0)
+}
