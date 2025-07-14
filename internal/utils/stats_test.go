1a75d6918f04e0595048cee07adc65227d13ae58
Martin Arias <arias95martin@gmail.com>
2025-07-13 21:25:45 -0300

    feat: Add statistics calculation tests

--- internal/utils/stats_test.go
+++ internal/utils/stats_test.go
@@ -8,14 +8,27 @@ import (
 
 var _ = Describe("Statistics calculation", func() {
 
-	It("should return mean, mode and median", func() {
-
+	It("should return mean, mode and median for an array of odd length", func() {
 		points := []int{200, 100, 20, 20, 80}
-
 		mean, median, mode := utils.CalculateStatistics(points)
-
 		Expect(mean).To(Equal(float64(84)))
 		Expect(median).To(Equal(float64(80)))
+		Expect(mode).To(Equal([]int{20}))
+	})
+
+	It("should return zero for all values", func() {
+		points := []int{}
+		mean, median, mode := utils.CalculateStatistics(points)
+		Expect(mean).To(Equal(float64(0)))
+		Expect(median).To(Equal(float64(0)))
+		Expect(mode).To(Equal([]int{}))
+	})
+
+	It("should return mean, mode and median for an array of odd length", func() {
+		points := []int{200, 100, 20, 30, 20, 80}
+		mean, median, mode := utils.CalculateStatistics(points)
+		Expect(mean).To(Equal(float64(75)))
+		Expect(median).To(Equal(float64(55)))
 		Expect(mode).To(Equal([]int{20}))
 	})
 
