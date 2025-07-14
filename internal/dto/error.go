71784e49154e6df14138501960648bec7ed47983
Martin Arias <arias95martin@gmail.com>
2025-07-14 17:27:18 -0300

    fix: Error message example value

--- internal/dto/error.go
+++ internal/dto/error.go
@@ -2,5 +2,5 @@ package dto
 
 // ErrorResponse defines a standard error response.
 type ErrorResponse struct {
-	Error string `json:"error" example:"invalid request"`
+	Error string `json:"error" example:"error message"`
 }
